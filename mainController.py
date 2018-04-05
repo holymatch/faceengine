18b9a9e2cc79823c4c5e1a7f2536bc4c0f7c71cf
holymatch <holymatch@gmail.com>
2018-04-05 16:02:25 +0800

    Update mainController.py
    Update for connect to faceweb

--- mainController.py
+++ mainController.py
@@ -5,11 +5,12 @@ from sqlalchemy import create_engine
 from flask_jsonpify import jsonify
 import base64
 import face_recognition
-from os import listdir
+from os import listdir, path, remove
 from os.path import isfile, join, splitext
 import tempfile
 import timeit
 import logging
+import numpy
 
 db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
@@ -19,28 +20,7 @@ know_face_path = "D:/OpenFace/FaceDB/know_face"
 # Cache the encoding
 known_encodings = []
 found_know_faces = []
-logging.basicConfig(level=logging.DEBUG)
-
-
-def load_know_face(identify):
-    with open(join(know_face_path, str(identify)+'.jpg'), "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        return encoded_string
-
-
-def save_know_face(face_data, identify):
-    with open(join(know_face_path, str(identify)+'.jpg'), "wb") as fh:
-        fh.write(base64.b64decode(face_data))
-
-
-class NewFace(Resource):
-    def post(self):
-        data = request.get_json()
-        logging.debug("Is Json {0}".format(request.is_json) )
-        logging.debug("Request is {0}, request.get_json() is {1}".format(request, request.get_json()))
-        save_know_face(data['FaceData'], data['Identify'])
-        return jsonify({"ReturnCode": 200, "Message": "Known face added"})
-        # Fetches first column that is Employee ID
+logging.basicConfig(level=logging.INFO)
 
 
 class Tracks(Resource):
@@ -49,12 +29,6 @@ class Tracks(Resource):
         query = conn.execute("select trackid, name, composer, unitprice from tracks;")
         result = {'data': [dict(zip(tuple(query.keys()), i)) for i in query.cursor]}
         return jsonify(result)
-
-
-class KnowFace(Resource):
-    def get(self, face_id):
-        result = {"face": {"identify": face_id, "faceData": load_know_face(face_id)}}
-        return jsonify(result.get("face "))
 
 
 class RecognizePerson(Resource):
@@ -71,23 +45,72 @@ class RecognizePerson(Resource):
             return jsonify(result)
 
 
+class FaceController(Resource):
+
+    def post(self):
+        data = request.get_json()
+        logging.debug("Is Json {0}".format(request.is_json) )
+        logging.debug("Request is {0}, request.get_json() is {1}".format(request, request.get_json()))
+        faces = encode_face(data['FaceData'])
+        if len(faces) > 0:
+            save_know_face_encode(data['FaceData'], data['Identify'])
+            return jsonify({"ReturnCode": 200, "Message": "Known face added"})
+        else:
+            return {"ReturnCode": 404, "Message": "No face found in input image"}
+
+    def delete(self, face_id):
+        face_file = join(know_face_path, str(face_id)+'.dat')
+        if path.exists(face_file):
+            try:
+                remove(face_file)
+                return 200
+            except OSError as e:
+                return 500
+        return 500
+
+
+def load_know_face(identify):
+    with open(join(know_face_path, str(identify)+'.jpg'), "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+        return encoded_string
+
+
+def save_know_face(face_data, identify):
+    with open(join(know_face_path, str(identify)+'.jpg'), "wb") as fh:
+        fh.write(base64.b64decode(face_data))
+
+
+def save_know_face_encode(face_data, identify):
+    face = encode_face(face_data)[0]
+    numpy.save(join(know_face_path, str(identify)+'.dat'), face, True)
+
+
+def load_know_face_encode(identify):
+    return numpy.load(join(know_face_path, str(identify)+'.dat.npy'))
+
+
 def load_faces_from_base64_image(image_str):
     with tempfile.NamedTemporaryFile() as tf:
         tf.write(base64.b64decode(image_str))
         return face_recognition.load_image_file(tf)
 
 
-def recognition(image):
-    total_start = timeit.default_timer()
+def encode_face(image):
     start = timeit.default_timer()
-    unknown_faces = face_recognition.face_encodings(load_faces_from_base64_image(image))
+    faces = face_recognition.face_encodings(load_faces_from_base64_image(image))
     end = timeit.default_timer()
     logging.info("Total process time of encode unknown faces: %s", str(end - start))
+    return faces
+
+
+def recognition(image):
+    total_start = timeit.default_timer()
+    unknown_faces = encode_face(image)
     if len(unknown_faces) > 0:
         image_to_test_encoding = unknown_faces[0]
     else:
         return {"ReturnCode": 404, "Message": "No face found in input image"}
-    know_face_list = [f for f in listdir(know_face_path) if isfile(join(know_face_path, f))]
+    know_face_list = [f for f in listdir(know_face_path) if isfile(join(know_face_path, f)) and f.endswith(".dat.npy")]
     # if the know face size is change, reload the encoding list
     logging.debug("Len of know_face_list {0} and known_encodings {1}".format(len(know_face_list), len(known_encodings)))
     if len(know_face_list) != len(known_encodings):
@@ -95,20 +118,19 @@ def recognition(image):
         found_know_faces[:] = []
         start = timeit.default_timer()
         for (i, face_image) in enumerate(know_face_list):
-            known_image = face_recognition.load_image_file(join(know_face_path, face_image))
-            found_face = face_recognition.face_encodings(known_image)
-            if len(found_face) > 0:
-                face_encoding = face_recognition.face_encodings(known_image)[0]
-                known_encodings.append(face_encoding)
-                found_know_faces.append(splitext(face_image)[0])
-            else:
-                logging.debug("No face found in {}.".format(face_image))
+            identify = splitext(splitext(face_image)[0])[0]
+            face_encoding = load_know_face_encode(identify)
+            logging.debug("face encoding: {}".format(face_encoding))
+            known_encodings.append(face_encoding)
+            found_know_faces.append(identify)
         logging.debug("Found know face {}".format(found_know_faces))
         logging.debug("Done loading know face")
         end = timeit.default_timer()
         logging.info("Total process time of encode faces: %s", str(end - start))
     # Check the distance of all face
     start = timeit.default_timer()
+    logging.debug("known_encodings {} ".format(known_encodings))
+    logging.debug("image_to_test_encoding {} ".format(image_to_test_encoding))
     face_distances = face_recognition.face_distance(known_encodings, image_to_test_encoding)
     end = timeit.default_timer()
     logging.info("Total process time of check face distances: %s", str(end - start))
@@ -136,9 +158,8 @@ def recognition(image):
     return face_result
 
 
-api.add_resource(NewFace, '/face')  # Route_1
 api.add_resource(Tracks, '/tracks')  # Route_2
-api.add_resource(KnowFace, '/face/<face_id>')  # Route_3
+api.add_resource(FaceController, '/face', '/face/<int:face_id>', endpoint='face_id')
 api.add_resource(RecognizePerson, '/recognize')
 
 
